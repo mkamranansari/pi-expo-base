7d76b6dcbd10690632aeebc49d5c0783942366ef
Muhammad Kamran Ansari <kansari921@gmail.com>
2021-11-13 19:29:50 +0500

    init

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>Hello !</Text>
+      <Text>lorem ipsum dolor sit amet !</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
